030cd6e51716f4795ee6907d1d342c1a831bc65e
Bengin Yilmaz <141424348+benginy@users.noreply.github.com>
2024-08-10 16:08:56 +0200

    Watch list heroku corrections

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,3 +1,11 @@
 Cloudinary.config do |config|
   config.cloudinary_url = ENV['CLOUDINARY_URL']
 end
+
+
+Cloudinary.config do |config|
+  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key = ENV['CLOUDINARY_API_KEY']
+  config.api_secret = ENV['CLOUDINARY_API_SECRET']
+  config.secure = true
+end
